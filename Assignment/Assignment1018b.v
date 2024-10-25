0b40222d1a75bd65a0285ed21cf8dd624877394e
tubehao <guanhao0412@163.com>
2024-10-25 08:20:09 +0800

    assignment 1018

--- Assignment/Assignment1018b.v
+++ Assignment/Assignment1018b.v
@@ -15,12 +15,16 @@ Definition opposite_sgn (x y: Z): Prop := x * y < 0.
 Fact opposite_sgn_plus_2: forall x,
   opposite_sgn (x + 2) x ->
   x = -1.
-Admitted. (* 请删除这一行_[Admitted]_并填入你的证明，以_[Qed]_结束。 *)
+Proof.
+  unfold opposite_sgn, square. nia.
+Qed.
+
 
 Fact opposite_sgn_odds: forall x,
   opposite_sgn (square x) x ->
   x < 0.
-Admitted. (* 请删除这一行_[Admitted]_并填入你的证明，以_[Qed]_结束。 *)
+unfold opposite_sgn, square. nia.
+Qed. (* 请删除这一行_[Admitted]_并填入你的证明，以_[Qed]_结束。 *)
 
 
 (************)
@@ -37,20 +41,35 @@ Lemma scale_quad_nonneg: forall a b c k: Z,
   k > 0 ->
   quad_nonneg a b c ->
   quad_nonneg (k * a) (k * b) (k * c).
-Admitted. (* 请删除这一行_[Admitted]_并填入你的证明，以_[Qed]_结束。 *)
+Proof.
+  unfold quad_nonneg.
+  intros.
+  pose proof H0 x.
+  nia.
+Qed.
 
 Lemma descale_quad_nonneg: forall a b c k: Z,
   k > 0 ->
   quad_nonneg (k * a) (k * b) (k * c) ->
   quad_nonneg a b c.
-Admitted. (* 请删除这一行_[Admitted]_并填入你的证明，以_[Qed]_结束。 *)
+Proof.
+  unfold quad_nonneg.
+  intros.
+  pose proof H0 x.
+  nia.
+Qed.
 
 Lemma plus_quad_nonneg: forall a1 b1 c1 a2 b2 c2: Z,
   quad_nonneg a1 b1 c1 ->
   quad_nonneg a2 b2 c2 ->
   quad_nonneg (a1 + a2) (b1 + b2) (c1 + c2).
-Admitted. (* 请删除这一行_[Admitted]_并填入你的证明，以_[Qed]_结束。 *)
-
+Proof.
+  unfold quad_nonneg.
+  intros.
+  pose proof H x.
+  pose proof H0 x.
+  nia.
+Qed.
 (** 我们知道，如果二次式的二次项系数为正且判别式不为正，那么这个二次式在自变量取遍一切
     实数的时候都恒为正。相应的性质在自变量取遍一切整数的时候自然也成立。请证明这一结
     论。【选做】*)
@@ -59,12 +78,20 @@ Lemma plus_quad_discriminant: forall a b c,
   a > 0 ->
   b * b - 4 * a * c <= 0 ->
   quad_nonneg a b c.
-Admitted. (* 请删除这一行_[Admitted]_并填入你的证明，以_[Qed]_结束。 *)
-
+Proof.
+  intros. 
+  unfold quad_nonneg.
+  intros x.
+  pose proof sqr_pos (2*a*x+b).
+  nia.
+Qed.
 (** 然而，判别式不为正并不是_[quad_nonneg]_的必要条件。下面命题是一个简单的反例。*)
 
 Example quad_nonneg_1_1_0: quad_nonneg 1 1 0.
-Admitted. (* 请删除这一行_[Admitted]_并填入你的证明，以_[Qed]_结束。 *)
+Proof.
+  unfold quad_nonneg. intros. nia.
+Qed.
+
 
 
 (************)
@@ -76,7 +103,7 @@ Admitted. (* 请删除这一行_[Admitted]_并填入你的证明，以_[Qed]_结
 Fact shift_up1_eq: forall f,
   shift_up1 f = func_plus f (fun x => 1).
 Proof.
-Admitted. (* 请删除这一行_[Admitted]_并填入你的证明，以_[Qed]_结束。 *)
-
+  intros. unfold shift_up1, func_plus. reflexivity.
+Qed. (* 请删除这一行_[Admitted]_并填入你的证明，以_[Qed]_结束。 *)
 
 
